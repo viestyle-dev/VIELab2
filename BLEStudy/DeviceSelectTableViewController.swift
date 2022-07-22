fcd0b309257558d58b50a2a98c3a193bb8ed037f
miokato <miokato@me.com>
2022-07-22 17:43:27 +0900

    Remove unused property.

--- BLEStudy/DeviceSelectTableViewController.swift
+++ BLEStudy/DeviceSelectTableViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class DeviceSelectTableViewController: UITableViewController {
     
-    var discoverDevices = [String]()
-    
     var timer: Timer?
 
     override func viewDidLoad() {
